8d4274a65fbdc5388af9c5124a78641a1ffdfedb
Julien Adam <julien.adam@gmail.com>
2023-01-07 14:00:17 +0100

    Traduction en français en cours

--- LINQ.Exercises/01_Element.cs
+++ LINQ.Exercises/01_Element.cs
@@ -1,27 +1,27 @@
-﻿
-using System.Linq;
+﻿using System.Linq;
 using Xunit;
 
 namespace LINQ.Exercises
 {
     /// <summary>
-    /// Your task is to apply LINQ `First/FirstOrDefault/Last/LastOrDefault` methods, so all tests will be passed.
-    /// Make sure to preview test data located in TestData.cs file.
-    /// Don't modify assertions or test data, all you need to do is to apply LINQ method so `result` variable will have expected value(s).
+    /// L'objectif ici est d'utiliser `First/FirstOrDefault/Last/LastOrDefault`
+    /// Inspectez les données situées dans le fichier TestData.cs
+    /// Ne modifiez pas les assertions ou les données de test ! Il faut utiliser les méthodes LINQ de manière à ce que 
+    /// la variable `result` contienne des données qui passent les tests.
     /// </summary>
     public class Element
     {
         [Fact]
-        public void First_n()
+        public void Premier_nombre()
         {
-            // First test is solved to show you how to use these exercises.
+            // Ce test passe pour vous montrer comment procéder
             int result = TestData.Numbers.First();
 
             Assert.Equal(1, result);
         }
 
         [Fact]
-        public void First_n_less_than_0()
+        public void Premier_nombre_plus_petit_que_zero()
         {
             int result = TestData.Numbers.First();
 
@@ -29,15 +29,15 @@ namespace LINQ.Exercises
         }
 
         [Fact]
-        public void Last_n_greater_than_0()
+        public void Dernier_nombre_plus_grand_que_0()
         {
-            int result = TestData.Numbers.Last();
+            int result = TestData.Numbers.First();
 
             Assert.Equal(5, result);
         }
 
         [Fact]
-        public void First_even_n()
+        public void Premier_nombre_pair()
         {
             int result = TestData.Numbers.First();
 
@@ -45,15 +45,15 @@ namespace LINQ.Exercises
         }
 
         [Fact]
-        public void Last_even_n()
+        public void Dernier_nombre_pair()
         {
-            int result = TestData.Numbers.Last();
+            int result = TestData.Numbers.First();
 
             Assert.Equal(-4, result);
         }
 
         [Fact]
-        public void First_n_greater_than_10_if_not_found_return_0()
+        public void Premier_nombre_plus_grand_que_10_si_pas_de_resultat_retourner_0()
         {
             int result = TestData.Numbers.First();
 
@@ -61,23 +61,23 @@ namespace LINQ.Exercises
         }
 
         [Fact]
-        public void Last_n_less_than_minus_1234_if_not_found_return_0()
+        public void Dernier_nombre_plus_petit_que_moins_1234_si_pas_de_resultat_retourner_0()
         {
-            int result = TestData.Numbers.Last();
+            int result = TestData.Numbers.First();
 
             Assert.Equal(0, result);
         }
 
         [Fact]
-        public void Last_elephant()
+        public void Dernier_elephant()
         {
-            string result = TestData.Animals.Last();
+            string result = TestData.Animals.First();
 
             Assert.Equal("elephant", result);
         }
 
         [Fact]
-        public void First_string_having_4_letters()
+        public void Premiere_chaine_de_4_caracteres()
         {
             string result = TestData.Animals.First();
 
@@ -85,15 +85,15 @@ namespace LINQ.Exercises
         }
 
         [Fact]
-        public void Last_string_containg_g()
+        public void Derniere_chaine_contenant_un_g()
         {
-            string result = TestData.Animals.Last();
+            string result = TestData.Animals.First();
 
             Assert.Equal("penguin", result);
         }
 
         [Fact]
-        public void First_string_having_s_as_first_letter()
+        public void Premiere_chaine_dont_la_premiere_lettre_est_un_s()
         {
             string result = TestData.Animals.First();
 
@@ -101,15 +101,15 @@ namespace LINQ.Exercises
         }
 
         [Fact]
-        public void Last_three_letter_long_word_or_null()
+        public void Dernier_mot_de_3_lettres_si_pas_de_resultat_retourner_null()
         {
-            string result = TestData.Animals.Last(s => s.Length == 3);
+            string result = TestData.Animals.First();
 
-            Assert.Equal(null, result);
+            Assert.Null(result);
         }
 
         [Fact]
-        public void First_person_born_after_2000()
+        public void Premiere_personne_nee_apres_2000()
         {
             TestData.Person result = TestData.People.First();
 
@@ -117,15 +117,7 @@ namespace LINQ.Exercises
         }
 
         [Fact]
-        public void Last_person_with_lastname_ending_with_l()
-        {
-            TestData.Person result = TestData.People.Last();
-
-            Assert.Equal(TestData.People[2], result);
-        }
-
-        [Fact]
-        public void First_person_born_141th_day_of_year()
+        public void Derniere_personne_dont_le_nom_de_famille_finit_par_un_l()
         {
             TestData.Person result = TestData.People.First();
 
@@ -133,11 +125,19 @@ namespace LINQ.Exercises
         }
 
         [Fact]
-        public void Last_person_whose_firstname_does_not_start_with_J_or_null()
+        public void Premiere_personne_nee_le_141_jour_de_l_annee()
         {
-            TestData.Person result = TestData.People.Last();
+            TestData.Person result = TestData.People.First();
 
-            Assert.Equal(null, result);
+            Assert.Equal(TestData.People[2], result);
+        }
+
+        [Fact]
+        public void Derniere_personne_dont_le_prenom_ne_commence_pas_par_J_ou_null_si_pas_de_resultat()
+        {
+            TestData.Person result = TestData.People.First();
+
+            Assert.Null(result);
         }
     }
 }
---
+++ LINQ.Exercises/02_Filtrage.cs
@@ -0,0 +1,175 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace LINQ.Exercises
+{
+    /// <summary>
+    /// Utiliser Where pour filtrer de manière à faire passer les tests
+    /// Inspectez les données situées dans le fichier TestData.cs
+    /// Ne modifiez pas les assertions ou les données de test ! Il faut utiliser les méthodes LINQ de manière à ce que 
+    /// la variable `result` contienne des données qui passent les tests.
+    /// </summary>
+    public class Filtrage
+    {
+        [Fact]
+        public void Nombres_plus_grands_que_1()
+        {
+            // Ce premier test passe
+            var result = TestData.Numbers.Where(n => n > 1);
+
+            Assert.Equal(3, result.Count());
+        }
+
+        [Fact]
+        public void Nombres_plus_petits_ou_egaux_a_zero()
+        {
+            var result = TestData.Numbers.Where(n => n <= 0);
+
+            Assert.Equal(new[] { -3, -1, -4, -1, -5 }, result);
+        }
+
+        [Fact]
+        public void N_fois_2_superieur_a_50()
+        {
+            var result = TestData.Numbers;
+
+            Assert.Equal(new[] { 3, 5 }, result);
+        }
+
+        [Fact]
+        public void N_est_pair()
+        {
+            var result = TestData.Numbers;
+
+            Assert.Equal(new[] { 2, -4 }, result);
+        }
+
+        [Fact]
+        public void L_index_de_n_est_impair()
+        {
+            var result = TestData.Numbers;
+
+            Assert.Equal(new[] { 1, 1, 3, 5 }, result);
+        }
+
+        [Fact]
+        public void N_est_impair_et_plus_petit_que_zero()
+        {
+            var result = TestData.Numbers;
+
+            Assert.Equal(new[] { -4 }, result);
+        }
+
+        [Fact]
+        public void N_au_carre_moins_2_fois_n_est_plus_grand_que_n()
+        {
+            var result = TestData.Numbers;
+
+            Assert.Equal(new[] { -3, -1, -4, -1, 5, -5 }, result);
+        }
+
+        [Fact]
+        public void Chaines_de_longueur_inferieure_a_5()
+        {
+            var result = TestData.Animals;
+
+            Assert.Equal(new[] { "lion" }, result);
+        }
+
+        [Fact]
+        public void Chaines_de_longueur_9()
+        {
+            var result = TestData.Animals;
+
+            Assert.Equal(new[] { "swordfish" }, result);
+        }
+
+        [Fact]
+        public void Chaines_commencant_par_s()
+        {
+            var result = TestData.Animals;
+
+            Assert.Equal(new[] { "swordfish", "shark" }, result);
+        }
+
+        [Fact]
+        public void Chaines_dont_la_deuxieme_lettre_est_un_i()
+        {
+            var result = TestData.Animals;
+
+            Assert.Equal(new[] { "tiger", "lion" }, result);
+        }
+
+        [Fact]
+        public void Chaines_contenant_un_e()
+        {
+            var result = TestData.Animals;
+
+            Assert.Equal(new[] { "tiger", "penguin", "elephant" }, result);
+        }
+
+        [Fact]
+        public void Chaines_terminant_par_un_t()
+        {
+            var result = TestData.Animals;
+
+            Assert.Equal(new[] { "elephant" }, result);
+        }
+
+        [Fact]
+        public void Chaines_contenant_la_chaine_io()
+        {
+            var result = TestData.Animals;
+
+            Assert.Equal(new[] { "lion" }, result);
+        }
+
+        [Fact]
+        public void Personnes_dont_le_nom_de_famille_et_le_prenom_commencent_par_la_meme_lettre()
+        {
+            var result = TestData.People;
+
+            Assert.Equal(new[] { TestData.People[3] }, result);
+        }
+
+        [Fact]
+        public void Personnes_nees_apres_1990()
+        {
+            var result = TestData.People;
+
+            Assert.Equal(new[] { TestData.People[1], TestData.People[3] }, result);
+        }
+
+        [Fact]
+        public void Personnes_nees_un_jour_pair()
+        {
+            var result = TestData.People;
+
+            Assert.Equal(new[] { TestData.People[0], TestData.People[3] }, result);
+        }
+
+        [Fact]
+        public void Personnes_nees_un_lundi_21()
+        {
+            var result = TestData.People;
+
+            Assert.Equal(new[] { TestData.People[2] }, result);
+        }
+
+        [Fact]
+        public void Personnes_ayant_18_ans_ou_plus_en_2000()
+        {
+            var result = TestData.People;
+
+            Assert.Equal(new[] { TestData.People[1], TestData.People[3] }, result);
+        }
+
+        [Fact]
+        public void Personnes_dont_le_nom_de_famille_contient_ll_et_dont_la_somme_jour_moi_annee_est_superieure_a_2000()
+        {
+            var result = TestData.People;
+
+            Assert.Equal(new[] { TestData.People[2] }, result);
+        }
+    }
+}
