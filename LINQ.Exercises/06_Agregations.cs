5ff5eda3f4957cf8173181fce2044e2c89d9a7db
Julien Adam <julien.adam@arcodia.fr>
2023-01-18 18:24:49 +0100

    Solution for aggregations

--- LINQ.Exercises/06_Agregations.cs
+++ LINQ.Exercises/06_Agregations.cs
@@ -26,7 +26,7 @@ namespace LINQ.Exercises
         [Fact]
         public void Nombre_d_occurences_de_1()
         {
-            int result = TestData.Numbers.Count();
+            int result = TestData.Numbers.Count(n => n == 1);
 
             Check.That(result).IsEqualTo(2);
         }
@@ -34,7 +34,7 @@ namespace LINQ.Exercises
         [Fact]
         public void Nombre_d_animaux_dont_le_nom_fait_5_lettres()
         {
-            int result = TestData.Animals.Count();
+            int result = TestData.Animals.Count(a => a.Length == 5);
 
             Check.That(result).IsEqualTo(2);
         }
@@ -42,7 +42,7 @@ namespace LINQ.Exercises
         [Fact]
         public void Somme_des_nombres()
         {
-            int result = TestData.Numbers.Count();
+            int result = TestData.Numbers.Sum();
 
             Check.That(result).IsEqualTo(-2);
         }
@@ -50,7 +50,7 @@ namespace LINQ.Exercises
         [Fact]
         public void Somme_des_longueurs_des_noms()
         {
-            int result = TestData.Animals.Count();
+            int result = TestData.Animals.Sum(a => a.Length);
 
             Check.That(result).IsEqualTo(38);
         }
@@ -58,7 +58,7 @@ namespace LINQ.Exercises
         [Fact]
         public void Somme_des_annees_de_naissance()
         {
-            int result = TestData.People.Count();
+            int result = TestData.People.Sum(p => p.Born.Year);
 
             Check.That(result).IsEqualTo(7915);
         }
@@ -66,7 +66,7 @@ namespace LINQ.Exercises
         [Fact]
         public void Nombre_le_plus_petit()
         {
-            int result = TestData.Numbers.Count();
+            int result = TestData.Numbers.Min();
 
             Check.That(result).IsEqualTo(-5);
         }
@@ -74,7 +74,7 @@ namespace LINQ.Exercises
         [Fact]
         public void Longueur_de_chaine_minimale()
         {
-            int result = TestData.Animals.Count();
+            int result = TestData.Animals.Min(a => a.Length);
 
             Check.That(result).IsEqualTo(4);
         }
@@ -82,7 +82,7 @@ namespace LINQ.Exercises
         [Fact]
         public void Animal_dont_le_nom_est_le_plus_long()
         {
-            string result = TestData.Animals.First();
+            string result = TestData.Animals.MaxBy(x => x.Length);
 
             Check.That(result).IsEqualTo("swordfish");
         }
@@ -90,7 +90,7 @@ namespace LINQ.Exercises
         [Fact]
         public void Personne_la_plus_agee()
         {
-            var result = TestData.People.First();
+            var result = TestData.People.MinBy(p => p.Born);
 
             Check.That(result).IsEqualTo(new TestData.Person("Jean", "Gean", new DateTime(1950, 12, 1)));
         }
@@ -98,7 +98,7 @@ namespace LINQ.Exercises
         [Fact]
         public void Nombre_le_plus_grand()
         {
-            int result = TestData.Numbers.Count();
+            int result = TestData.Numbers.Max();
 
             Check.That(result).IsEqualTo(5);
         }
@@ -106,7 +106,7 @@ namespace LINQ.Exercises
         [Fact]
         public void Longueur_de_nom_d_animal_la_plus_grande()
         {
-            int result = TestData.Animals.Count();
+            int result = TestData.Animals.Max(a => a.Length);
 
             Check.That(result).IsEqualTo(9);
         }
@@ -114,7 +114,7 @@ namespace LINQ.Exercises
         [Fact]
         public void Personne_la_plus_jeune()
         {
-            var result = TestData.People.First();
+            var result = TestData.People.MaxBy(p => p.Born);
 
             Check.That(result).IsEqualTo(new TestData.Person("Jill", "Lill", new DateTime(2001, 5, 21)));
         }
@@ -122,7 +122,7 @@ namespace LINQ.Exercises
         [Fact]
         public void Moyenne_des_nombres()
         {
-            double result = TestData.Numbers.Count();
+            double result = TestData.Numbers.Average();
 
             Check.That(result).IsEqualTo(-0.2);
         }
@@ -130,7 +130,7 @@ namespace LINQ.Exercises
         [Fact]
         public void Moyenne_des_annéees_de_naissance()
         {
-            double result = TestData.People.Count();
+            double result = TestData.People.Average(p => p.Born.Year);
 
             Check.That(result).IsEqualTo(1978.75);
         }
@@ -138,7 +138,7 @@ namespace LINQ.Exercises
         [Fact]
         public void Produit_de_tous_les_nombres()
         {
-            int result = TestData.Numbers.First();
+            int result = TestData.Numbers.Aggregate((state, item) => state * item);
 
             Check.That(result).IsEqualTo(-1800);
         }
@@ -153,7 +153,17 @@ namespace LINQ.Exercises
             // Si le jour est supérieur à 15, soustraire 10
             // Sinon, ajouter 5
             // Ajouter le résultat au total
-            int result = TestData.People.Aggregate(0, (sum, person) => 1);
+            int result = TestData.People.Aggregate(256, (sum, person) =>
+            {
+                if (person.Born.Day > 15)
+                {
+                    return sum + person.Born.Day - 10;
+                }
+                else
+                {
+                    return sum + person.Born.Day + 5;
+                }
+            });
 
             Check.That(result).IsEqualTo(296);
         }
